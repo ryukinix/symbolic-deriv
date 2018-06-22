d7ee8a93504bab48e49f61132e6601a304d4e286
Manoel Vilela <manoel_vilela@engineer.com>
2018-06-22 16:14:39 -0300

    Fix docstring of infix module

--- infix.rkt
+++ infix.rkt
@@ -5,7 +5,7 @@
 (define operators-precedence '(^ * / + -))
 
 (define (prefix->infix exp)
-  "Translate prefix notation to infix notation: (* 2 3) => (2 * 3)"
+  "Translate prefix notation to infix notation: (* 2 3) => (2 * 3)."
   (cond ((null? exp) '())
         ((atom? exp) exp)
         (else (list (prefix->infix (arg1 exp))
@@ -14,7 +14,7 @@
 
 ;; FIXME: this algorithm is just a Left-to-Right parser without precedence
 (define (infix->prefix exp)
-  "Parse infix expression to infix: (3 + (2 ^ 3)) => (+ 3 (^ 2 3)) "
+  "Parse infix expression to infix: (3 + (2 ^ 3)) => (+ 3 (^ 2 3))."
   (cond ((atom? exp) exp)
         ((null? (cdr exp)) (infix->prefix (car exp)))
         (else (list (cadr exp)
@@ -22,5 +22,5 @@
                     (infix->prefix (cddr exp))))))
 
 (define (deriv->infix exp var)
-  "Derivation using infix notation as output"
+  "Derivation using infix notation as input and output."
   (prefix->infix (deriv (infix->prefix exp) var)))

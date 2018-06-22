d469de04cc9375075d2bbffe44b5210aa07442ab
Manoel Vilela <manoel_vilela@engineer.com>
2018-06-22 04:53:04 -0300

    Add division rule and simplify main algorithm
    Add as well little comment boxes to help divide the code.

--- deriv.rkt
+++ deriv.rkt
@@ -13,7 +13,7 @@ Implemented rules:
 
 + Subtraction
 + Product
-+ Quotient/Division (not yet)
++ Quotient/Division
 + Sum
 + Power
 + Exponential of e
@@ -24,6 +24,11 @@ Implemented rules:
 (define arg2 caddr)
 (define ^ expt) ;; power symbol function
 
+#|
+
+== CHECKERS ==
+
+|#
 
 (define (atom? exp)
   "Check if a EXP it's a simple atom: that means is not a pair-cons cell."
@@ -59,10 +64,12 @@ Implemented rules:
        (eq? (car exp) op)))
 
 (define (binary-op? exp op)
+  "Use operation? procedure with a constraint of exp being a binary operation"
   (and (operation? exp op)
        (= (length exp) 3)))
 
 (define (unary-op? exp op)
+  "Use operation? procedure with a constraint of exp being a unary operation"
   (and (operation? exp op)
        (= (length exp) 2)))
 
@@ -80,20 +87,29 @@ Implemented rules:
 
 (define (product? exp)
   "Check if is a product expression."
-  (operation? exp '*))
+  (binary-op? exp '*))
+
+(define (division? exp)
+  (binary-op? exp '/))
 
 (define (power? exp var)
   "Check if EXP it's operator is a power ^ and not a exponential."
-  (and (operation? exp '^)
+  (and (binary-op? exp '^)
        (contains-var? (arg1 exp) var)
        (or (constant? (arg2 exp) var)
            (not (contains-var? (arg2 exp) var)))))
 
 (define (exponential? exp var)
   "Check if EXP is a exponential expression of VAR."
-  (and (operation? exp '^)
+  (and (binary-op? exp '^)
        (eq? (arg1 exp) 'e)
        (contains-var? (arg2 exp) var)))
+
+#|
+
+== SYMBOLIC EXPRESSIONS CONSTRUCTIONS ==
+
+|#
 
 (define (make-sum a1 a2)
   "Make a sum expression based in a1 and a2 already simplified."
@@ -126,17 +142,20 @@ Implemented rules:
 (define (make-product m1 m2)
   "Make a product expression simplified based in m1 and m2."
   (cond ((and (number? m1)
-              (number? m2))
-         (* m1 m2))
+              (number? m2)) (* m1 m2))
         ((or (and (number? m1) (= m1 0))
-             (and (number? m2) (= m2 0)))
-         0)
-        ((and (number? m1) (= m1 1))
-         m2)
+             (and (number? m2) (= m2 0))) 0)
+        ((and (number? m1) (= m1 1)) m2)
 
-        ((and (number? m2) (= m2 1))
-         m1)
+        ((and (number? m2) (= m2 1)) m1)
         (else (list '* m1 m2))))
+
+(define (make-division a b)
+  "Make a division expression based in a and b."
+  (cond ((and (number? a)
+              (number? b)) (/ a b))
+        ((and (number? b) (= b 1)) a)
+        (else (list '/ a b))))
 
 (define (make-power base pow)
   "Make a product expression simplified of BASE and POW."
@@ -144,8 +163,19 @@ Implemented rules:
         ((and (number? base) (= base 1)) 1)
         (else (list '^ base pow))))
 
+#|
+
+== CALCULUS DERIVATION RULES ==
+
+|#
+
+(define (sum-rule exp var)
+  "Definition of the sum rule: f(x) + g(x) => f'(x) + g'(x)"
+  (make-sum (deriv (arg1 exp) var)
+            (deriv (arg2 exp) var)))
+
 (define (power-rule exp var)
-  "Definition of the power-rule: x^n => n*x^(n-1)"
+  "Definition of the power rule: x^n => n*x^(n-1)"
   (let* ((base (arg1 exp))
          (pow (arg2 exp))
          (new-pow (if (number? pow)
@@ -155,30 +185,39 @@ Implemented rules:
                                 pow
                                 (deriv base var)))))
 
+(define (product-rule exp var)
+  "Definition of the product rule: f(x)g(x) => f'(x)g(x) + f(x)g'(x)"
+  (make-sum (make-product (arg1 exp)
+                          (deriv (arg2 exp) var))
+            (make-product (arg2 exp)
+                          (deriv (arg1 exp) var))))
+
+(define (division-rule exp var)
+  "Definition of the division rule: f(x)/g(x) => (f'(x)g(x)-f(x)g'(x))/(g(x))²"
+  (let ((f (arg1 exp))
+        (g (arg2 exp)))
+    (make-division (make-subtraction (make-product (deriv f var) g)
+                                     (make-product f (deriv g var)))
+                   (make-power g 2))))
+
 (define (exponential-rule exp var)
   "Use of the chain-rule for the exponential: e^p(x) => p'(x)*e^p(x)"
   (let ((base (arg1 exp))
         (pow (arg2 exp)))
     (make-product (deriv pow var) (make-power base pow))))
 
+;; MAIN ALGORITHM
 
 (define (deriv exp var)
   "Main function with calculus rules for derivation."
   (cond ((constant? exp var) 0)
         ((same-var? exp var) 1)
-        ((sum? exp)
-         (make-sum (deriv (arg1 exp) var)
-                   (deriv (arg2 exp) var)))
-        ((binary-subtraction? exp)
-         (make-subtraction (deriv (arg1 exp) var)
-                           (deriv (arg2 exp) var)))
-        ((unary-subtraction? exp)
-         (list '- (deriv (arg1 exp) var)))
-        ((product? exp)
-         (make-sum (make-product (arg1 exp)
-                                 (deriv (arg2 exp) var))
-                   (make-product (arg2 exp)
-                                 (deriv (arg1 exp) var))))
+        ((sum? exp) (sum-rule exp var))
+        ((binary-subtraction? exp) (make-subtraction (deriv (arg1 exp) var)
+                                                     (deriv (arg2 exp) var)))
+        ((unary-subtraction? exp) (list '- (deriv (arg1 exp) var)))
+        ((product? exp) (product-rule exp var))
+        ((division? exp) (division-rule exp var))
         ((power? exp var) (power-rule exp var))
         ((exponential? exp var) (exponential-rule exp var))
         (else (error (format "Unknown derivation rule for exp: ~a." exp)))))
